d6fb5f80186885af83a4b55bed7009a4b5024b1f
ddmetro <dpadnani@gmail.com>
2013-03-06 22:53:30 +0530

    Create Node.java
    class representing a node in DLX matrix

---
+++ Node.java
@@ -0,0 +1,22 @@
+public class Node
+{
+    Node left;
+	Node right;
+	Node up;
+	Node down;
+	Node columnHead;
+	String name;
+	int size;
+
+	public Node(String name, int size, Node columnHead)
+	{
+		this.name = name;
+		this.size = size;
+		this.columnHead = columnHead;
+	}
+
+	public String toString()
+	{
+		return name;
+	}
+}
